785ee3ef39acb8e06bf994f1717f6cc3c14eae7d
MuhammetSaid <muhammetsaidelsalih@gmail.com>
2025-12-13 14:13:58 +0300

    feature/db-olusturma

--- DB/app/models/__init__.py
+++ DB/app/models/__init__.py
@@ -1,5 +1,6 @@
 from .user import User
 from .rating import Rating
 from .level import Level
+from .ad_reward import AdReward
 
-__all__ = ["User", "Rating", "Level"]
+__all__ = ["User", "Rating", "Level", "AdReward"]
